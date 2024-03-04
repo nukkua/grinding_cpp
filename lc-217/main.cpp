a497b9ea8b4d485f156077a13a0643347058fcab
nukkua <marco.salazar.2014.com@gmail.com>
2024-03-04 12:06:04 -0400

    217lc

---
+++ lc-217/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+auto constains_duplicate(std::vector<int> &nums) noexcept -> bool {
+  bool it_contains = true;
+  std::unordered_set<int> nums2;
+  for (int i = 0; i < nums.size(); ++i) {
+    nums2.insert(nums[i]);
+  }
+
+  if (nums.size() == nums2.size()) {
+    it_contains = false;
+  }
+
+  return it_contains;
+}
+
+auto contains_duplicate_one_line(std::vector<int> &nums) noexcept -> bool {
+  return nums.size() > std::set<int>(nums.begin(), nums.end()).size();
+}
+
+bool contains_duplicate_unordered_map(std::vector<int> &nums) {
+  std::unordered_map<int, int> mp;
+  for (const auto &x : nums) {
+    mp[x]++;
+  }
+  bool flag = false;
+  for (const auto &i : mp) {
+    if (i.second >= 2)
+      return true;
+  }
+  return flag;
+}
+
+auto main() -> int {
+  int n;
+  std::cin >> n;
+  std::vector<int> nums(n);
+
+  for (int i = 0; i < n; ++i) {
+    std::cin >> nums[i];
+  }
+  std::cout << "Results: ";
+  std::cout << "\n";
+  std::cout << contains_duplicate_one_line(nums);
+  std::cout << "\n";
+  std::cout << constains_duplicate(nums);
+  std::cout << "\n";
+  std::cout << contains_duplicate_unordered_map(nums);
+  std::cout << "\n";
+
+  return 0;
+}
