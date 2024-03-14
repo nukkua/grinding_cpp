9e73aa09c2e2e2399c315ed090ef7d92bda9611b
nukkua <marco.salazar.2014.com@gmail.com>
2024-03-14 09:58:09 -0400

    done loco

--- lc-238/main.cpp
+++ lc-238/main.cpp
@@ -2,21 +2,36 @@
 #include <vector>
 
 std::vector<int> productExceptSelf(std::vector<int> &nums) {
-  std::vector<int> sol(nums.size());
-
   int n = nums.size();
-  int multiplier_1 = 1;
+  std::vector<int> sol(n);
+  sol[0] = 1;
+  for (int i = 1; i < n; ++i) {
+    sol[i] = sol[i - 1] * nums[i - 1];
+  }
+
+  int right_verifier = 1;
+
+  for (int i = n - 1; i >= 0; --i) {
+    sol[i] = sol[i] * right_verifier;
+    right_verifier *= nums[i];
+  }
+
+  return sol;
+}
+
+auto productExceptSelf_brute_force(std::vector<int> &nums) -> std::vector<int> {
+  int n = nums.size();
+  std::vector<int> sol(n);
+  int multiplier = 1;
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      if (i != j) {
-        multiplier_1 *= nums[j];
-      }
+      if (i == j)
+        continue;
+      multiplier *= nums[i];
     }
-    sol[i] = multiplier_1;
-    multiplier_1 = 1;
+    sol[i] = multiplier;
   }
-
   return sol;
 }
 
